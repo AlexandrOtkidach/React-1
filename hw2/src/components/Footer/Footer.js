ad8795b79704c734e0d3a6628b4f38ff10a8007e
Alexandr Otkidach <Alexandr.otkidach@gmail.com>
2022-05-02 23:23:38 +0300

    main

--- hw2/src/components/Footer/Footer.js
+++ hw2/src/components/Footer/Footer.js
@@ -2,9 +2,9 @@ export function Footer () {
     return(
         <footer className="footer-block">
            <ul>
-               <li><a href>React</a></li>
-               <li><a href>Mozila Developer Network</a></li>
-               <li><a href>My GitHub Profile</a></li>
+               <li><a href="google.com">React</a></li>
+               <li><a href="google.com">Mozila Developer Network</a></li>
+               <li><a href="google.com">My GitHub Profile</a></li>
            </ul>
            <div className='footer-text'>
                 2022, Kiev, Ukrain
--- hw2/src/components/Header/Header.js
+++ hw2/src/components/Header/Header.js
@@ -5,9 +5,9 @@ export function Header () {
      <header className="header-block">
         <img src={logo} alt="Pumba" className="header-logo"/>
         <ul>
-            <li><a href>Home</a></li>
-            <li><a href>About</a></li>
-            <li><a href>Contact</a></li>
+            <li><a href="google.com">Home</a></li>
+            <li><a href="google.com">About</a></li>
+            <li><a href="google.com">Contact</a></li>
         </ul>
      </header>
  )
